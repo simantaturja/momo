1fe0f68433181d9736e7bb8e3bfffae154cdcf2e
Simanta Deb Turja <simantaturja@gmail.com>
2026-07-16 13:29:06 +0600

    release: bump version to 0.2.0

--- Tests/MomoCoreTests/SmokeTests.swift
+++ Tests/MomoCoreTests/SmokeTests.swift
@@ -3,6 +3,6 @@ import XCTest
 
 final class SmokeTests: XCTestCase {
     func testVersionExists() {
-        XCTAssertEqual(MomoCore.version, "0.1.0")
+        XCTAssertEqual(MomoCore.version, "0.2.0")
     }
 }
